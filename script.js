b2b2689a50e68e959d2762bd634a7cde6cb19be1
Pavel <pavel.pawlowski@gmail.com>
2020-11-12 17:38:31 +0300

    'Тест'

--- script.js
+++ script.js
@@ -21,5 +21,26 @@
 
 Проверить, чтобы все работало без ошибок в консоли */
 
-'use strict';
+" use strict";
 
+const numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
+
+
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt ('Один из последних просмотренных фильмов?',''),
+      b = prompt('На сколько оцените его?',''),
+      c = prompt ('Один из последних просмотренных фильмов?',''),
+      d = prompt('На сколько оцените его?','');
+
+personalMovieDB.movies[a] = b; 
+personalMovieDB.movies[c] =d;  
+
+console.log(personalMovieDB);
